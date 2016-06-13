00c8b188e9c1a7fe2366c49d6549e4f5f82a662d
floalex <flo.liang@my.com>
2016-06-12 23:35:29 -0700

    Refactor validation code

--- todo.rb
+++ todo.rb
@@ -32,9 +32,37 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? {|list| list[:name] == name}
+    "List name must be unique."
+  end
+end
+
 # Create a new list
 post "/lists" do
-  session[:lists] << {name: params[:list_name], todos: []}
-  session[:success] = "The list has been created"
-  redirect "/lists"
+  list_name = params[:list_name].strip
+
+  if error = error_for_list_name(list_name)
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+  # # if list_name.size >= 1 && list_name.size <= 100
+  # if !(1..100).cover? list_name.size #use cover to ensure value is within the range
+  #   session[:error] = "List name must be between 1 and 100 characters."
+  #   erb :new_list, layout: :layout
+  # elsif session[:lists].any? { |list| list[:name] == list_name }
+  #   session[:error] = "List name must be unique."
+  #   erb :new_list, layout: :layout
+  # else
+  #   session[:lists] << {name: list_name, todos: []}
+  #   session[:success] = "The list has been created"
+  #   redirect "/lists"    
+  # end
 end
