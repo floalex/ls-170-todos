0cb0c2bf5ec228c01780cdd40df55bb266093a89
floalex <flo.liang@my.com>
2016-06-13 14:52:57 -0700

    edit list

--- todo.rb
+++ todo.rb
@@ -75,3 +75,28 @@ get "/lists/:id" do
   @list = session[:lists][id]
   erb :list, layout: :layout
 end
+
+# Edit an existing todo list
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# Update an existing todo list
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  error = error_for_list_name(list_name)
+
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{id}"
+  end
+end
