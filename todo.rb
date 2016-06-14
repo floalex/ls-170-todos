616e73589cb269bdd2f9a1306c55efe13abd24e7
floalex <flo.liang@my.com>
2016-06-13 22:54:23 -0700

    delete todo items

--- todo.rb
+++ todo.rb
@@ -42,6 +42,13 @@ def error_for_list_name(name)
   end
 end
 
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_todo(name)
+  if !(1..100).cover? name.size
+    "Todo name must be between 1 and 100 characters."
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
@@ -71,8 +78,8 @@ end
 
 # Display a single todo list
 get "/lists/:id" do
-  id = params[:id].to_i
-  @list = session[:lists][id]
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
   erb :list, layout: :layout
 end
 
@@ -88,8 +95,8 @@ post "/lists/:id" do
   list_name = params[:list_name].strip
   id = params[:id].to_i
   @list = session[:lists][id]
+  
   error = error_for_list_name(list_name)
-
   if error
     session[:error] = error
     erb :edit_list, layout: :layout
@@ -106,4 +113,22 @@ post "/lists/:id/destroy" do
   session[:lists].delete_at(id)
   session[:success] = "The list has been deleted."
   redirect "/lists"
+end
+
+# Add a new todo to a list
+post "/lists/:list_id/todos" do
+  # first thing is to find the list
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << {name: text, completed: false}
+    session[:success] = "The todo item has been added."
+    redirect "/lists/#{@list_id}"
+  end
 end
