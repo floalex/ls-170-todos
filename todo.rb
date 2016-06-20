e641f6a9bacbe8e486822f81409bb3b73aeeaa4c
floalex <flo.liang@my.com>
2016-06-19 21:03:33 -0700

    Adding Identifiers to Lists

--- todo.rb
+++ todo.rb
@@ -29,8 +29,8 @@ helpers do
   def sort_lists(lists, &block)
     complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list)}
 
-    incomplete_lists.each { |list| yield list, lists.index(list) }
-    complete_lists.each { |list| yield list, lists.index(list) }
+    incomplete_lists.each(&block)
+    complete_lists.each(&block)
     # older solution:
     # incomplete_lists = {}
     # complete_lists = {}
@@ -50,16 +50,37 @@ helpers do
   def sort_todos(todos, &block)
     complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed]}
 
-    incomplete_todos.each { |todo| yield todo }
-    complete_todos.each { |todo| yield todo }
+    incomplete_todos.each(&block)
+    complete_todos.each(&block)
   end
 
-  def load_list(index)
-    list = session[:lists][index] if index
+  def load_list(id)
+    list = session[:lists].find { |list| list[:id] == id }
     return list if list 
 
     session[:error] = "The specified list was not found."
     redirect "/lists"
+  end
+
+  def next_element_id(elements)
+    max = elements.map { |todo| todo[:id] }.max || 0
+    max + 1
+  end
+
+  # Return an error message if the name is invalid. Return nil if name is valid.
+  def error_for_list_name(name)
+    if !(1..100).cover? name.size
+      "List name must be between 1 and 100 characters."
+    elsif session[:lists].any? {|list| list[:name] == name}
+      "List name must be unique."
+    end
+  end
+
+  # Return an error message if the name is invalid. Return nil if name is valid.
+  def error_for_todo(name)
+    if !(1..100).cover? name.size
+      "Todo name must be between 1 and 100 characters."
+    end
   end
 end
 
@@ -88,22 +109,6 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
-# Return an error message if the name is invalid. Return nil if name is valid.
-def error_for_list_name(name)
-  if !(1..100).cover? name.size
-    "List name must be between 1 and 100 characters."
-  elsif session[:lists].any? {|list| list[:name] == name}
-    "List name must be unique."
-  end
-end
-
-# Return an error message if the name is invalid. Return nil if name is valid.
-def error_for_todo(name)
-  if !(1..100).cover? name.size
-    "Todo name must be between 1 and 100 characters."
-  end
-end
-
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
@@ -113,7 +118,8 @@ post "/lists" do
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << {name: list_name, todos: []}
+    id = next_element_id(session[:lists])
+    session[:lists] << {id: id, name: list_name, todos: []}
     session[:success] = "The list has been created."
     redirect "/lists"
   end
@@ -141,7 +147,7 @@ end
 # Edit an existing todo list
 get "/lists/:id/edit" do
   id = params[:id].to_i
-  @list = session[:lists][id]
+  @list = load_list(id)
   erb :edit_list, layout: :layout
 end
 
@@ -149,7 +155,7 @@ end
 post "/lists/:id" do
   list_name = params[:list_name].strip
   id = params[:id].to_i
-  @list = session[:lists][id]
+  @list = load_list(id)
   
   error = error_for_list_name(list_name)
   if error
@@ -165,18 +171,13 @@ end
 # Delete a todo list
 post "/lists/:id/destroy" do
   id = params[:id].to_i
-  session[:lists].delete_at(id)
+  session[:lists].reject! { |list| list[:id] == id }
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     "/lists"
   else
     session[:success] = "The list has been deleted."
     redirect "/lists"
   end
-end
-
-def next_todo_id(todos)
-  max = todos.map { |todo| todo[:id] }.max || 0
-  max + 1
 end
 
 # Add a new todo to a list
@@ -191,7 +192,7 @@ post "/lists/:list_id/todos" do
     session[:error] = error
     erb :list, layout: :layout
   else
-    id = next_todo_id(@list[:todos])
+    id = next_element_id(@list[:todos])
     @list[:todos] << { id: id, name: text, completed: false}
     
     session[:success] = "The todo item has been added."
